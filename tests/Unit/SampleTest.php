c1db3f227a132ec16a56ea27a8a35f38e6c0cc00
Gourab Sahu <gourab@mail.uni-paderborn.de>
2020-12-09 23:39:21 +0100

    Initial development for Symfony testing

---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+   /** @test **/
+   public function firstTest()
+   {
+       $this->assertEquals('1','1');
+
+   }
+}
